c38ce8ed70d5527fd9e632a81d6f47d80d955031
skdrums <sora.kannaduki1009@gmail.com>
2024-07-05 15:26:56 +0900

    fix docker file, add put api, fix readme to explain how to push image to ecr

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 # from infer.detection.exec import text
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool = None
 
 @app.get("/")
 def read_root():
@@ -12,3 +18,8 @@ def read_root():
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
+# @app.post("/items")
+
+@app.put("/items/{item_id}")
+def put_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}

955644c7352fbfd5e91600bc92c7e1b2d00d64cb
skdrums <sora.kannaduki1009@gmail.com>
2024-07-22 11:16:00 +0900

    fix lint

--- src/util/logger.py
+++ src/util/logger.py
@@ -1,5 +1,6 @@
 import datetime
-import watchtower, logging
+import watchtower
+import logging
 import os
 
 logging.basicConfig(level=logging.INFO)
@@ -7,11 +8,14 @@ _logger = logging.getLogger('sample_logger')
 if os.environ['ENVIRONMENT'] != 'local':
     _logger.addHandler(watchtower.CloudWatchLogHandler())
 
+
 def info(message):
     _logger.info('[%s] %s' % (current_datetime(), message))
 
+
 def error(message):
     _logger.error('[%s] %s' % (current_datetime(), message))
+
 
 def current_datetime():
     return datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')
